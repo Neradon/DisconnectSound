6ae226a1f232cb2e084eade52160831ca5b951c7
Edward7 <ediro174@gmail.com>
2022-08-13 15:28:37 +0300

    Added MP3 Support and made some small changes

--- DisconnectSound.cs
+++ DisconnectSound.cs
@@ -1,68 +1,75 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using ABI_RC.Core.Networking;
-using DarkRift.Client;
 using MelonLoader;
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Net;
-
-using MelonLoader;
-using System;
-using System.Collections;
-using UnityEngine.Networking;
-using Object = UnityEngine.Object;
+using System.Linq;
 
 namespace DisconnectSound
 {
     public class DisconnectSound : MelonMod
     {
         private AudioSource _audioSource;
-        private void Test(object sender, DisconnectedEventArgs e)
+        private void Disconnect(object sender, DarkRift.Client.DisconnectedEventArgs e)
         {
             if (e == null) return;
-             LoggerInstance.Msg("Playing Disconnected Sound");
-             _audioSource.Play();
+            LoggerInstance.Msg("Playing Disconnected Sound");
+            _audioSource.Play();
         }
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             if (buildIndex == 3)
-                MelonCoroutines.Start(InitAudio());  
+                MelonCoroutines.Start(InitAudio());
         }
-
         private IEnumerator InitAudio()
         {
+            if (!Directory.Exists(Directory.GetCurrentDirectory() + "//DisconnectedSound"))
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + "//DisconnectedSound");
 
-
-            if (File.Exists("UserData/dc.ogg"))
+            if (Directory.GetFiles(Directory.GetCurrentDirectory() + "//DisconnectedSound").Length == 0)
             {
-                using (var uwr = UnityWebRequestMultimedia.GetAudioClip($"file://{Path.Combine(Environment.CurrentDirectory, "UserData/dc.ogg")}", AudioType.OGGVORBIS)) {
+                using (WebClient wc = new WebClient())
+                    wc.DownloadFile("https://github.com/Edward7s/DisconnectSound/raw/main/Sound/BipSound.mp3", Directory.GetCurrentDirectory() + "//DisconnectedSound//Audio.mp3");
+            }
+            string path = Directory.GetFiles(Directory.GetCurrentDirectory() + "//DisconnectedSound").FirstOrDefault();
+
+            if (path.EndsWith(".mp3"))
+            {
+                WWW webReq = new WWW("File://" + path);
+                yield return webReq;
+                _audioSource = GameObject.Find("SteamManager").AddComponent<AudioSource>();
+                GenerateAudioSource(webReq.GetAudioClip(false, false));
+                yield break;
+            }
+            if (path.EndsWith(".ogg"))
+            {
+                using (var uwr = UnityWebRequestMultimedia.GetAudioClip(path, AudioType.OGGVORBIS))
+                {
                     yield return uwr.SendWebRequest();
-                    if (uwr.isNetworkError || uwr.isHttpError) {
-                        Debug.LogError(uwr.error);
+                    if (uwr.isNetworkError || uwr.isHttpError)
+                    {
+                        LoggerInstance.Error(uwr.error);
                         yield break;
                     }
                     AudioClip testAudio = DownloadHandlerAudioClip.GetContent(uwr);
                     testAudio.hideFlags |= HideFlags.DontUnloadUnusedAsset;
-                    _audioSource = GameObject.Find("SteamManager").AddComponent<AudioSource>();
-                    _audioSource.clip = testAudio;
-                    _audioSource.volume = 0.35f;
-                    NetworkManager.Instance.GameNetwork.Disconnected += Test;
+                    GenerateAudioSource(testAudio);
+                    yield break;
                 }
             }
-            else
-            {
-                LoggerInstance.Warning("Please put a dc.ogg in the UserData folder");
-            }
-            
-
-                
-                
-
+            LoggerInstance.Warning("Please put a dc.ogg in the UserData folder");
         }
 
+        private void GenerateAudioSource(AudioClip audioClip)
+        {
+            _audioSource = GameObject.Find("SteamManager").AddComponent<AudioSource>();
+            _audioSource.clip = audioClip;
+            _audioSource.volume = 0.35f;
+            NetworkManager.Instance.GameNetwork.Disconnected += Disconnect;
+        }
     }
 }
